b561832113e0c8f49812bdc6bc39dcc9ca9634ad
Evandro Verdino <evandroverdino@gmail.com>
2016-02-18 18:01:54 -0300

    Fixing Sending Data 18/01

--- app/src/main/java/br/com/gods/mathcalendar/communication/MathCalendarServiceCallback.java
+++
@@ -1,8 +0,0 @@
-package br.com.gods.mathcalendar.communication;
-
-
-public interface MathCalendarServiceCallback<T> {
-
-    void success(T response);
-    void failure(MathCalendarServiceError error);
-}
--- app/src/main/java/br/com/gods/mathcalendar/communication/UserService.java
+++
@@ -1,6 +0,0 @@
-package br.com.gods.mathcalendar.communication;
-
-public interface UserService {
-
-    void userData(String name/*, String age, String email, String vip*/, MathCalendarServiceCallback<Response> callback);
-}
